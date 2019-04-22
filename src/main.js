338a788c8f25e1a5b5d0437a2026e0a035f3a065
MrKKHub <1298878065@qq.com>
2019-04-22 17:07:56 +0800

    change

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 // 解决移动端300毫秒点击延迟问题
 fastClick.attach(document.body)
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop:true
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/bd0af35db8de5fff3379cc082fd21db6.jpg_750x200_5ea5bcce.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/26c9b54e3e02d3724f614883d77a62c6.jpg_750x200_57aa8efa.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background-color: #fff;
+}
+
+.wrapper {
+  width: 100%;
+  height: 0;
+  padding-bottom: 27%;
+  overflow: hidden;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
